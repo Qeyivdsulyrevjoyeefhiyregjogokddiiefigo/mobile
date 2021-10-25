d35228637be5f7efc3ca25870bb9bfd03ba4caff
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-10-25 08:35:05 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ donasi hanya via doa bismillahirrahmanirrahim semoga yang baca bisa sukses dan dapat hidayah di kehidupan nya...
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi', m)
+`.trim(), '𝚁𝙴𝚃𝙴𝙰𝙼.𝙸𝙳', 'Donasi', '.donasi', m)
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i

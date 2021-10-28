37ea79c83ca301f06ecdb8d68200cf72b3060522
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-10-28 19:28:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -34,8 +34,8 @@ global.packname = 'FOLLOW IG'
 global.author = 'https://www.instagram.com/reteam.id/'
 
 global.wait = '_*Wait A Minutes...*_'
-global.eror = '_*Server Error*_'
-global.fla = 'https://telegra.ph/file/b08bd29e4f773d10b69c6.jpg'
+global.eror = '_*Terjadi Kesalahan*_'
+global.fla = 'https://telegra.ph/file/0aad74310cbe2812e7af3.jpg'
 
 global.multiplier = 69 // Semakin tinggi, semakin sulit naik level
 

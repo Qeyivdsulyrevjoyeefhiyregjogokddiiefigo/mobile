ba25c6edc555cd9e36eef9b5bf2d8dc4605d3c0e
Qeyivdsulyrevjoyeefhiyregjogokddiiefigo <92343572+Qeyivdsulyrevjoyeefhiyregjogokddiiefigo@users.noreply.github.com>
2021-11-02 05:53:24 +0700

    Update family100.js

--- plugins/family100.js
+++ plugins/family100.js
@@ -1,5 +1,5 @@
 let fetch = require('node-fetch')
-let winScore = 500
+let winScore = 5000
 async function handler(m) {
     this.game = this.game ? this.game : {}
     let id = 'family100_' + m.chat
